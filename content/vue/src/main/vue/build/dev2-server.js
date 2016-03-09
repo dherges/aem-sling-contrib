98ef650bca85af443567d8f0e605c8ab0a783105
dherges <david@spektrakel.de>
2016-03-09 18:23:55 +0100

    vue.js demo: vue cli + vue-loader + webpack + webpack dev server

---
+++ content/vue/src/main/vue/build/dev2-server.js
@@ -0,0 +1,57 @@
+/*
+ * aem-sling-contrib
+ * https://github.com/dherges/aem-sling-contrib
+ *
+ * Copyright (c) 2016 David Herges
+ * Licensed under the MIT license.
+ */
+
+var express = require('express')
+var webpack = require('webpack')
+var config = require('./webpack.dev2.conf')
+var proxy = require('express-http-proxy')
+
+var app = express()
+var compiler = webpack(config)
+
+var devMiddleware = require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath,
+  stats: {
+    colors: true,
+    chunks: false
+  }
+})
+
+var hotMiddleware = require('webpack-hot-middleware')(compiler)
+// force page reload when html-webpack-plugin template changes
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' })
+    cb()
+  })
+})
+
+// handle fallback for HTML5 history API
+app.use(require('connect-history-api-fallback')())
+// serve webpack bundle output
+app.use(devMiddleware)
+// enable hot-reload and state-preserving
+// compilation error display
+app.use(hotMiddleware)
+// serve pure static assets
+app.use('/static', express.static('./static'))
+
+// proxy to sling instance
+app.use('/api', proxy('localhost:4502', {
+  forwardPath: function(req, res) {
+    return require('url').parse(req.url).path;
+  }
+}))
+
+app.listen(8080, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('Listening at http://localhost:8080')
+})
